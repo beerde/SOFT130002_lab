bd320fb1eefd4bd4273a691ce716fec8df244086
beerdee <18302010053@fudan.edu.cn>
2019-05-02 16:09:40 +0800

    更改lab6

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,18 +1,23 @@
 window.onload= function () {
     document.getElementById("p1").onclick=function () {
         document.getElementById("bigPicture").src="images/medium/5855774224.jpg";
+        document.getElementById("mouseEle").innerHTML=document.getElementById("p1").title;
     }
     document.getElementById("p2").onclick=function () {
         document.getElementById("bigPicture").src="images/medium/5856697109.jpg";
+        document.getElementById("mouseEle").innerHTML=document.getElementById("p2").title;
     }
     document.getElementById("p3").onclick=function () {
         document.getElementById("bigPicture").src="images/medium/6119130918.jpg";
+        document.getElementById("mouseEle").innerHTML=document.getElementById("p3").title;
     }
     document.getElementById("p4").onclick=function () {
         document.getElementById("bigPicture").src="images/medium/8711645510.jpg";
+        document.getElementById("mouseEle").innerHTML=document.getElementById("p4").title;
     }
     document.getElementById("p5").onclick=function () {
         document.getElementById("bigPicture").src="images/medium/9504449928.jpg";
+        document.getElementById("mouseEle").innerHTML=document.getElementById("p5").title;
     }
     document.getElementById("featured").onmouseenter= function () {
         document.getElementById("mouseEle").style.opacity=0.8;
